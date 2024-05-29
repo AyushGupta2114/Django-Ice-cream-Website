1a92e48444b0b679d994fa07db6fb8a6cab01c46
AyushGupta2114 <ayushgupta220503@gmail.com>
2024-05-30 02:32:46 +0530

    data base will be updated as per form

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 def index(request):
     context={
@@ -17,5 +19,11 @@ def services(request):
 
 def contact(request):
     # return HttpResponse("This is contact page")
-    
-     return render(request,'contact.html')
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+    return render(request,'contact.html')
